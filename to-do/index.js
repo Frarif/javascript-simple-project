d7a8854336651401d161017a934fd0fd132af2ae
M Haidar Hanif <git@mhaidarhanif.com>
2023-01-25 21:57:46 +0700

    refactor: login flow with less conditional checks

--- to-do/index.js
+++ to-do/index.js
@@ -1,46 +1,41 @@
 // To-Do
-function dailyActivities() {
-  const myactivities = [
-    "Sleep",
-    "Breakfast",
-    "Workout",
-    "Go to work",
-    "Lunch",
-    "Home",
-  ];
-  const addActivity = prompt(`Add new activity:`);
-  myactivities.push(addActivity);
-  console.log(myactivities);
+
+const myActivities = [
+  "Sleep",
+  "Breakfast",
+  "Workout",
+  "Go to work",
+  "Lunch",
+  "Home",
+];
+
+function addNewActivity() {
+  const newActivity = prompt(`Add new activity:`);
+  myActivities.push(newActivity);
+  console.log(myActivities);
 }
 
-// LOGIN
 function runLogin() {
   const username = prompt("Username:");
   const password = prompt("Password:");
-  if (username == "arif") {
-    if (password == "250297") {
-      alert("Selamat datang Mas Arif.");
-      dailyActivities();
-    } else if (password == "") {
-      alert("Password belum diisi, silahkan isi.");
-    } else {
-      alert("Password salah, coba lagi!");
-    }
-  } else if (password == "250297") {
-    if (username == "arif") {
-      alert("Selamat datang Mas Arif.");
-      dailyActivities();
-    } else if (username == "") {
-      alert("Username anda belum diiisi, silahkan isi.");
-    } else {
-      alert("Username anda salah, coba lagi!");
-    }
-  } else if (username == "" && password == "") {
-    alert("Username dan password anda belum diisi, silahkan diisi.");
-  } else if (username !== "arif" && password !== "250297") {
-    alert("Username dan password anda salah, silahkan coba lagi");
-  } else {
-    alert("Username dan password anda belum diisi, silahkan diisi.");
+
+  // If there is an error
+  if (!username || username === "") {
+    alert("Usename belum diisi, silahkan isi.");
+  }
+  if (!password || password === "") {
+    alert("Password belum diisi, silahkan isi.");
+  }
+
+  // If credentials are wrong
+  if (username !== "arif" || password !== "250297") {
+    alert("Username atau password salah, silahkan coba lagi");
+  }
+
+  // If everything is fine
+  if (username === "arif" && password === "250297") {
+    alert("Selamat datang Mas Arif.");
+    addNewActivity();
   }
 }
 
