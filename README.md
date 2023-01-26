9e343b85e33edae4a05b28092f68fa5e70bcebab
Frarif <freakinarif25@gmail.com>
2023-01-26 17:02:36 +0700

    create: new function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # javascript-simple-project
 
 This is for my simple JavaScript project
+
+# Get the employee with the highest salary
--- employee/index.js
+++ employee/index.js
@@ -1,54 +1,27 @@
 // Search Employee
 const employeesData = [
-  { id: 01, name: "Muhammad Ucok", salary: 5000000 },
-  { id: 02, name: "Butet Sulastri", salary: 6000000 },
-  { id: 03, name: "Sri Juminten", salary: 7000000 },
-  { id: 04, name: "Anto Mulyono", salary: 6000000 },
-  { id: 05, name: "Tukiyem Winata", salary: 8000000 },
-  { id: 06, name: "Parjo Wijaya", salary: 9000000 },
-  { id: 07, name: "Sumiati Angel", salary: 5000000 },
-  { id: 08, name: "Tukat Pardi", salary: 7000000 },
-  { id: 09, name: "Muhammad Ikhsan", salary: 8000000 },
+  { nip: 01, name: "Muhammad Ucok", salary: 5000000 },
+  { nip: 02, name: "Butet Sulastri", salary: 6000000 },
+  { nip: 03, name: "Sri Juminten", salary: 7000000 },
+  { nip: 04, name: "Anto Mulyono", salary: 6000000 },
+  { nip: 05, name: "Tukiyem Winata", salary: 8000000 },
+  { nip: 06, name: "Parjo Wijaya", salary: 9000000 },
+  { nip: 07, name: "Sumiati Angel", salary: 5000000 },
+  { nip: 08, name: "Tukat Pardi", salary: 7000000 },
+  { nip: 09, name: "Muhammad Ikhsan", salary: 8000000 },
+  { nip: 10, name: "Michael Yusuf", salary: 9000000 },
+  { nip: 11, name: "Ikhsan Miraka", salary: 5000000 },
+  { nip: 12, name: "Jokowi Udin", salary: 6000000 },
+  { nip: 13, name: "Wati Minkem", salary: 10000000 },
+  { nip: 14, name: "Erick Jaja", salary: 7000000 },
+  { nip: 15, name: "Dedi Suhendra", salary: 10000000 },
 ];
 
-function seacrhEmployees(employees, keyword) {
-  const foundStudents = employees.filter((employee) => {
-    return employee.name.toLowerCase().includes(keyword);
+function findHighestSalary(employees) {
+  const highestSalary = employees.splice((employee) => {
+    return employee.salary.length;
   });
-  return foundStudents;
+  console.log(highestSalary);
 }
 
-const foundEmployees = seacrhEmployees(employeesData, `a`);
-
-// LOGIN
-function runLogin() {
-  const username = prompt("Username:");
-  const password = prompt("Password:");
-  if (username == "arif") {
-    if (password == "250297") {
-      alert("Selamat datang Mas Arif.");
-      console.log("Found employee:", foundEmployees);
-    } else if (password == "") {
-      alert("Password belum diisi, silahkan isi.");
-    } else {
-      alert("Password salah, coba lagi!");
-    }
-  } else if (password == "250297") {
-    if (username == "arif") {
-      alert("Selamat datang Mas Arif.");
-      console.log("Found employee:", foundEmployees);
-    } else if (username == "") {
-      alert("Username anda belum diiisi, silahkan isi.");
-    } else {
-      alert("Username anda salah, coba lagi!");
-    }
-  } else if (username == "" && password == "") {
-    alert("Username dan password anda belum diisi, silahkan diisi.");
-  } else if (username !== "arif" && password !== "250297") {
-    alert("Username dan password anda salah, silahkan coba lagi");
-  } else {
-    alert("Username dan password anda belum diisi, silahkan diisi.");
-  }
-}
-
-runLogin();
+console.log(findHighestSalary(employeesData));
