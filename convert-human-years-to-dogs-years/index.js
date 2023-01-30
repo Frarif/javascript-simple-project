fe63a37c40107fa8e5f10e2d98697a0389292887
Frarif <freakinarif25@gmail.com>
2023-01-30 10:32:03 +0700

    Create: new page

---
+++ convert-human-years-to-dogs-years/index.js
@@ -0,0 +1,14 @@
+// Human's age
+var myAge = 22;
+// The first two year
+var earlyYears = 2 * 10.5;
+// Then
+let laterYears = myAge - 2;
+// Dog's year
+laterYears *= 10;
+// My age in dogs year
+var myAgeInDogYears = earlyYears + laterYears;
+var name = "Arif Setiawan".toLowerCase();
+console.log(
+  `My name is ${name}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`
+);
